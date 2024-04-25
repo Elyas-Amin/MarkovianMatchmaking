1a10921034d712a1f2083805e7130237f2752cff
Jeffrey Hsu <jeffreyhsu.08@gmail.com>
2024-04-25 12:42:04 -0700

    csp outline detailed

--- csp.py
+++ csp.py
@@ -1,11 +1,17 @@
 from Profile import Profile
 
 def mrv(profiles):
+    """Choose person with least number of preferences"""
 
 def forward_checking(csp):
-
+    """if domain becomes < 10, return failure"""
+    
 def backtracking(csp):
+    """if forward checking returns failure, update constraint thresholds"""
 
 def ac_3(csp):
+    """if one user rejects a profile, this will update the rejected profile's domain as necessary"""
+    #constraint acquistion??
 
 def csp(profile):
+    """Variables are each user, constraints are True/False and thresholds for heuristics"""
