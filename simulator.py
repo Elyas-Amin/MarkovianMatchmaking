597c5549a51b61e61155d168c45e837f3a2152fd
Jeffrey Hsu <jeffreyhsu.08@gmail.com>
2024-05-12 16:11:12 -0400

    updated simulator with sigmoid function

--- simulator.py
+++ simulator.py
@@ -1,38 +1,33 @@
 import numpy as np
-import stat
 from profile import Profile
 
-def decision(user: Profile, profile: Profile):
-    decision = 0
+class Simulator:
 
-    np.stat.normal(len())
+    def decision(self, user: Profile, profile: Profile):
+        '''Decisions are made based on compatibility scorefollowing a sigmoid function'''
+        compatibility_score = user.compute_compatibility(profile)
+        
+        # Define scaling factor and shifting constant
+        a = 1  # shape of the sigmoid
+        b = 0  # x-axis shift
 
-    return decision
+        # Calculate acceptance probability
+        acceptance_probability = 1 / (1 + np.exp(-a * compatibility_score + b))
+        
+        # Make decision based on acceptance probability
+        decision = np.random.choice([0, 1], p=[1 - acceptance_probability, acceptance_probability])
+        return decision
 
-def tick_simulation(user: Profile, profiles):
-    '''Tick-based simulation of user deciding on each profile from set of profiles'''
-    accepts = set()
-    rejects = set()
-    count = len(profiles)
+    def simulation(self, user: Profile, profiles):
+        '''Tick-based simulation of user deciding on each profile from set of profiles'''
+        accepts = set()
+        rejects = set()
 
+        while profiles:
+            profile = profiles.pop()
+            if self.decision(user, profile):
+                accepts.add(profile)
+            else:
+                rejects.add(profile)
 
-    while profiles:
-        profile = profiles.pop()
-        if decision(user, profile):
-            accepts.add(profile)
-        else:
-            rejects.add(profile)
-
-    return accepts, rejects
-
-if __name__ == '__main__':
-
-    # user to be making decision
-    #user = 
-    #profiles = 
-
-    # cluster of profiles to be decided on
-    #profiles = csp(user, profiles)
-
-    #print(tick_simulation(user, profiles))
-
+        return accepts, rejects
