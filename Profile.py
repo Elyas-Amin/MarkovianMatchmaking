722fe533282e562f27c43278e3cc01821d034574
Elyas Amin <elyasamin26@gmail.com>
2024-05-12 17:49:57 -0400

    Updated csp

--- Profile.py
+++ Profile.py
@@ -1,5 +1,5 @@
 class Profile:
-    def __init__(self, id: int, age: int, religion: str, location: str, zodiac:str, education_level: str):
+    def __init__(self, id: int, age: int, religion: str, location: str, zodiac:str, education_level: str, preferences):
         self.id = id
         self.age = age
         self.religion = religion
@@ -8,7 +8,7 @@ class Profile:
         self.education_level = education_level
         self.tags = set()
         self.compatabile_profiles = set()
-        self.preferences = set() 
+        self.preferences = preferences
         
     def compute_compatibility(self, match): #normalize comptability between 0 and 1
         tags_score = len(set.intersection(self.tags, match.tags))
--- csp.py
+++ csp.py
@@ -7,62 +7,85 @@ class CSP:
     def __init__(self):
         self.variables - {}
         self.values = {}
-        
 
-def isComplete(assigment):
-    return len(assigment) == TOTAL_USERS
+    # def isComplete(assigment):
+    #     return len(assigment) == TOTAL_USERS
+            
+    def isConsistent(self, user, var, constraints[var], potential_match):
+        #in backtracking relax the constraints
+        if var == "age": #age constraint should be single number
+            if abs(user.age - potential_match.age) <= constraints[var]:
+                return True
+        elif var == "religion":
+            if potential_match.va not in constraints[var]:
+                return False
+            return True
+        elif var == "location":
+            if potential_match.location not in constraints[var]:
+                return False
+            return True
+        elif var == "zodiac":
+            if potential_match.zodiac not in constraints[var]:
+                return False
+            return True
+        elif var == "education_level":
+            if potential_match.education_level not in constraints[var]:
+                return False
+            return True            
         
-def isConsistent(profiles, value, assignment, var):
-    #check if the compatability scores are within a certain range
-    if var.compute_compatibility(value) >= 0.5:
+        
+    def forward_checking(self, user, variables, var, potential_match, constraints, generated_profiles):
+        remaining_profiles = generated_profiles.copy() #not sure if needed, ask jeffrey
+        remaining_profiles.remove(potential_match) #remove current match
+        
+        if remaining_profiles < 10:
+            return False
+        
+        for neighbor in variables:
+            if neighbor == var: #skip current var
+                continue
+            #check if other var will reduce the overall remaining profiles to less than 10
+            if not self.isConsistent(user, neighbor, constraints[neighbor], potential_match):
+                return False
         return True
-    return False
 
-# def mrv(users):
-#     #Choose profile with least amount of prefernecs
-#     min = users[0]
-#     for x in users:
-#         if len(x.prefernces) < len(min.prefernces):
-#             min = x
-#     return min
-    
-# def forward_checking(csp):
-#     """if domain becomes < 10, return failure"""
-#   check cluster size
-    
-#check to see if fc is necessary for csp's
+    def constraint_acquisition(csp):
+        """if one user rejects a profile, this will update the rejected profile's domain as necessary"""
+        #constraint acquistion??
+        #something here
+        
+    def match_profiles(self, user, generated_profiles, matches = {}):
+                
+        variables = user.preferences.keys()
+        domain = generated_profiles.copy() #not sure if needed, ask jeffrey
+        domain.remove(user)
+        constraints = user.prefernces.copy() #not sure if needed, ask jeffrey
+            
+        for var in variables:
+            matches[var] = []    
+                
+        for potential_match in generated_profiles:
+            for var in variables: #choose next characteristic to match
+                if self.isConsistent(user, var, constraints[var], potential_match):
+                    matches[var].append(potential_match) #don't need to reduce domain, just add to matches
+                    inferences = self.forward_checking(user, variables, var, potential_match, constraints[var], generated_profiles)
+                    if inferences:
+                        result = match_profiles(user, domain, matches)
+                        if result:
+                            return result
+                    matches[var].remove(potential_match) #remove current assigment in fc fails
+            return False
 
-#
-    
-def backtracking_search(profiles):
-    assigment = {}
-    return backtracking(assigment, profiles)
 
-def constraint_acquisition(csp):
-    """if one user rejects a profile, this will update the rejected profile's domain as necessary"""
-    #constraint acquistion??
-    #something here
-    
+#########################################################################
 
-def csp(profile):
-    """Variables are each user, constraints are True/False and thresholds for heuristics"""
+def __main__(self):
+    generated_profiles = #call on profile generator here
     
+    matches = {}
     
-def backtracking(assigment, profiles):
-    """if forward checking returns failure, update constraint thresholds"""
-    if isComplete(assigment):
-        return assigment
-    var = mrv(assigment, profiles) #choose next user
-    for value in var:
-        if isConsistent(profiles, value, assigment, var): #if not consistent then remove profile
-            if var in assigment.keys():
-                assigment[var].append(value) #add new value to list
-            else: #createa a new entry with list value containing the profile to be added
-                assigment[var] = [value]
-            inferences = forward_checking(profiles, assigment, var, value)
-            if inferences:
-                result = backtracking(assigment, profiles)
-                if result:
-                    return result
-            del assigment[var].remove(value)
-    return False
+    for user in generated_profiles:
+        matches[user] = self.match_profiles(user, generated_profiles)
+    
+    return matches
+    
