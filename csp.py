3411ccb34ac11e3187cf80ba3bd282c5d2d1ab9a
Elyas Amin <elyasamin26@gmail.com>
2024-05-13 15:24:40 -0400

    fixed csp

--- csp.py
+++ csp.py
@@ -2,16 +2,18 @@ from Profile import Profile
 import sqlite3
 import json
 import random
+import timeit
+import time
 
-global TOTAL_USERS
-TOTAL_USERS = 1000
+START_TIME = 0
+STOP_TIME = 0
 
 class CSP:
     def __init__(self):
         self.variables = {}
         self.values = {}
 
-    def isConsistent(self, user, var, var_constraints, potential_match):
+    def isConsistent(self, user, var, var_constraints, potential_match): #checks if the preferences of user matches teh characteristics of potential match
         if var == "age_range":
             return abs(user.age - potential_match.age) <= var_constraints
         elif var == "religion_pref":
@@ -19,9 +21,9 @@ class CSP:
         # elif var == "location_pref":
         #     return potential_match.location == var_constraints
         elif var == "zodiac_pref":
-            return potential_match.zodiac in var_constraints
+            return potential_match.zodiac in var_constraints if not potential_match.preferences["zodiac_pref"] else True
         elif var == "education_pref":
-            return potential_match.education_level in var_constraints
+            return potential_match.education_level in var_constraints if not potential_match.preferences["education_pref"] else True
 
     def forward_checking(self, user, variables, var, potential_match, constraints, generated_profiles):
         # generated_profiles.remove(potential_match)
@@ -39,26 +41,25 @@ class CSP:
         # generated_profiles.append(potential_match)
         return True
 
-    def match_profiles(self, user, profiles, var_count, matches):
+    def match_profiles(self, user, profiles, var_count, potential_match, matches):
         constraints = user.preferences
-
-        if var_count == 3:
+        
+        if var_count == 3: #base case
             return matches
 
-        for potential_match in profiles:
-            for var in matches.keys():
-                if len(matches[var]) == 1 and potential_match in matches[var]:
-                    continue  # Skip if potential match is already assigned
-                var_constraints = constraints[var]
+        for var in matches.keys():
+            if len(matches[var]) >= 1 and potential_match in matches[var]:
+                continue  # Skip if potential match is already assigned
+            var_constraints = constraints[var]
 
-                if self.isConsistent(user, var, var_constraints, potential_match):
-                    matches[var].append(potential_match)
-                    inferences = self.forward_checking(user, matches.keys(), var, potential_match, constraints, profiles)
-                    if inferences:
-                        result = self.match_profiles(user, profiles, var_count + 1, matches)
-                        if result:
-                            return result
-                    matches[var].remove(potential_match)
+            if self.isConsistent(user, var, var_constraints, potential_match):
+                matches[var].append(potential_match)
+                inferences = self.forward_checking(user, matches.keys(), var, potential_match, constraints, profiles)
+                if inferences:
+                    result = self.match_profiles(user, profiles, var_count + 1, potential_match, matches)
+                    if result:
+                        return result
+                matches[var].remove(potential_match)
 
         return False
 
@@ -75,10 +76,14 @@ if __name__ == "__main__":
     # Select 1000 random profiles excluding the user profile
     # random_profiles = random.sample([id for id in all_profile_ids], 1000)
 
-    c.execute("SELECT * FROM profiles ORDER BY id LIMIT 10000")
+
+    START_TIME = timeit.default_timer()
+    c.execute("SELECT * FROM profiles ORDER BY id LIMIT 2000")
     random_profiles = c.fetchall()
+    print("done")
 
     profiles = []
+    counter1 =0
     for profile_tuple in random_profiles:
         profile_id = profile_tuple[0]  # Extracting the id from the tuple
         c.execute("SELECT * FROM profiles WHERE id=?", [profile_id])
@@ -86,11 +91,14 @@ if __name__ == "__main__":
         preferences = json.loads(profile_row[6])
         profile_instance = Profile(profile_row[0], profile_row[1], profile_row[2], profile_row[3], profile_row[4], profile_row[5], preferences)
         profiles.append(profile_instance)
+        counter1+=1
+        print(counter1)
 
     conn.close()
 
     matches = {}
 
+    counter = 0
     for user in profiles:
         user_matches = {
             "age_range": [],
@@ -102,20 +110,25 @@ if __name__ == "__main__":
         var_count = 0
         profile_copy = profiles.copy()
         profile_copy.remove(user)
-        matches[user] = csp.match_profiles(user, profile_copy, var_count, user_matches)
+        for potential_match in profile_copy:
+            matches[user] = csp.match_profiles(user, profile_copy, var_count, potential_match, user_matches)
+        counter +=1
+        print(counter)
+    STOP_TIME = timeit.default_timer()
 
-        # Printing the result
-        for user, match in matches.items():
-            if type(match) == bool:
-                continue
-            # print(len(match["age_range"]))
-            match_set = set()
-            for var, list in match.items():
-                for p in list:
-                    if p not in match_set:
-                        match_set.add(p)
-            
-            print(user.id, len(match_set))
+    # Printing the result
+    for user, match in matches.items():
+        if type(match) == bool:
+            print(user.id, 0)
+            continue
+        match_set = set()
+        for var, list in match.items():
+            for p in list:
+                if p not in match_set:
+                    match_set.add(p)
+        
+        print(user.id, len(match_set))
+    print(STOP_TIME-START_TIME)
 
 
 
