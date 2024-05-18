583cf522009611c1ce2584ccad442ef3f1b907a6
Jeffrey Hsu <jeffreyhsu.08@gmail.com>
2024-05-18 01:06:42 -0400

    updated drl

--- drl.py
+++ drl.py
@@ -1,23 +1,18 @@
 import numpy as np
-import tensorflow as tf
 import keras
-from Profile import Profile
 from simulator import Simulator
 import random
 from visualizer import QValuesVisualizer
-from retriever import Retriever
 import matplotlib.pyplot as plt
-from keras.callbacks import Callback
-from datetime import datetime
 
 import prof_char as p_char
 
 from generator import generate_profile
 
 class DQNAgent:
-    def __init__(self, state_size, action_size, max_memory_size=1000):
-        self.state_size = state_size
-        self.action_size = action_size
+    def __init__(self, max_memory_size=1000):
+        self.state_size = 30 # 30 features encoded in state
+        self.action_size = 2 # Agent can suggest or not suggest a profile
         self.memory = []
         self.max_memory_size = max_memory_size
         self.gamma = 0.95  # Discount rate
@@ -25,9 +20,6 @@ class DQNAgent:
         self.epsilon_min = 0.01
         self.epsilon_decay = 0.995
         self.model = self._build_model()
-        self.callbacks = [keras.callbacks.TensorBoard(log_dir='./logs')]
-        log_dir = "logs/" + datetime.now().strftime("%Y%m%d-%H%M%S")
-        self.tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
         self.losses = []
 
 
@@ -59,8 +51,8 @@ class DQNAgent:
 
         return np.concatenate((user_state, match_state))
 
-    def remember(self, state, action, reward, next_state, final):
-        self.memory.append((state, action, reward, next_state, final))
+    def remember(self, state, action, reward, next_state):
+        self.memory.append((state, action, reward, next_state))
         if len(self.memory) > self.max_memory_size:
             self.memory.pop(0)  # Remove the oldest experience
 
@@ -82,18 +74,16 @@ class DQNAgent:
         # Sample a minibatch of experiences from memory
         minibatch = random.sample(self.memory, batch_size)
 
-        for state, action, reward, next_state, final in minibatch:
+        for state, action, reward, next_state in minibatch:
 
             # Calculate the target value for the Q-network
-            target = reward # if experience was final state
-            if not final:
-                next_state = np.expand_dims(next_state, axis=0)
-                target = (reward + self.gamma * np.amax(self.model.predict(next_state, batch_size=None)[0]))
+            next_state = np.expand_dims(next_state, axis=0)
+            reward = (reward + self.gamma * np.amax(self.model.predict(next_state, batch_size=None)[0]))
             
             state = np.expand_dims(state, axis=0)
-            target_f = self.model.predict(state, batch_size=None) # Get the current Q-values for the state
-            target_f[0][action] = target # Update the Q-value for the selected action
-            history = self.model.fit(state, target_f, epochs=1, verbose=0)
+            q_value = self.model.predict(state, batch_size=None) # Get the current Q-values for the state
+            q_value[0][action] = reward # Update the Q-value for the selected action
+            history = self.model.fit(state, q_value, epochs=1, verbose=0)
             self.losses.append(history.history['loss'][0])
 
             # self.model.fit(state, target_f, epochs=1, verbose=0) # Train the Q-network using the updated Q-value
@@ -101,17 +91,55 @@ class DQNAgent:
         # Update epsilon (exploration rate) using epsilon decay
         if self.epsilon > self.epsilon_min:
             self.epsilon *= self.epsilon_decay
+
+    def unsupervised_learning(self, user, profiles, simulator):
+        suggested = 0
+        accepts = 0
+        while profiles:
+            # 20 profiles per episode
+            for _ in range(20):
+                if len(profiles) == 0:
+                    break
+
+                # Select an action using the agent's policy
+                match = profiles.pop()
+                state = self.encode_state(user, match)
+                action = self.act(state)
+                if action == 1:
+                    suggested += 1
+
+                    # Get user's response
+                    response = simulator.decision(user, match)
+                    reward = 100 if response else -5
+                    if response:
+                        accepts += 1
+
+                    # Encode the new state based on the user's response
+                    next_state = self.encode_state(user, match)
+
+                else:
+                    # If no profile is suggested, the state remains the same
+                    next_state = state
+                    reward = 0
+
+                # Remember the experience
+                agent.remember(state, action, reward, next_state)
+
+                # Set the current state to the new state
+                state = next_state
+
+            # Update the agent at the end of each episode
+            agent.replay(30)
+
+        return accepts, suggested
     
     def save_model(self, filename):
         self.model.save(filename)
 
 
 # Initialize the environment and DRL agent
-state_size = 30
-action_size = 2  # Assuming three possible profiles to suggest
-agent = DQNAgent(state_size, action_size)
+agent = DQNAgent()
 simulator = Simulator()
-visualizer = QValuesVisualizer()
 
 # # Fetch user and profiles from the database
 # retriever = Retriever()
@@ -119,85 +147,21 @@ visualizer = QValuesVisualizer()
 # for _ in range(1):
 #     training_set.append(retriever.random_profiles(1000))
 
-def encode_state(user_profile, suggested_profile):
-    # Encode user's age, education level, religion, zodiac sign
-    user_state = np.array([
-        user_profile.age,
-        p_char.e.index(user_profile.education_level),
-        p_char.r.index(user_profile.religion),
-        p_char.z.index(user_profile.zodiac)
-    ])
-
-    # Encode suggested profile's characteristics
-    suggested_state = np.zeros(len(p_char.r) + len(p_char.l) + len(p_char.z) + 1)  #  1 extra slots for compatibility score and age
-    suggested_state[p_char.r.index(suggested_profile.religion)] = 1
-    suggested_state[len(p_char.r) + p_char.l.index(suggested_profile.location)] = 1
-    suggested_state[len(p_char.r) + len(p_char.l) + p_char.z.index(suggested_profile.zodiac)] = 1
-
-    # Compute compatibility score and discretize it
-    suggested_state[len(suggested_state) - 1] = suggested_profile.age
-
-    return np.concatenate((user_state, suggested_state))
 
 user = generate_profile(p_char.r, p_char.l, p_char.z, p_char.e, p_char.t)
 profiles = set()
-print(user)
 
-for _ in range(100):
+for _ in range(1000):
     profiles.add(generate_profile(p_char.r, p_char.l, p_char.z, p_char.e, p_char.t))
 
-# Initialize the initial state to the user's state
-state = encode_state(user, profiles.pop())  # Assuming the first profile is the initial suggestion
+accepts, suggested = agent.unsupervised_learning(user, profiles, simulator)
 
-# Simulation loop
-episode_length = 10  # Number of profiles per episode
-suggested = 0
-accepts = 0
-while profiles:
-    for _ in range(episode_length):
-        if len(profiles) == 0:
-            break
-        # Select an action using the agent's policy
-        action = agent.act(state)
-        if action == 1:
-            suggested += 1
-
-            # Get user's response
-            suggested_profile = profiles.pop()
-            response = simulator.decision(user, suggested_profile)
-            reward = 10 if response else -5
-            if response:
-                accepts += 1
-
-            # Encode the new state based on the user's response
-            next_state = encode_state(user, suggested_profile)
-
-        else:
-            # If no profile is suggested, the state remains the same
-            next_state = state
-            reward = 0
-
-        # Determine if this is the end of the episode
-        final = _ == episode_length - 1  # Check if this is the last suggestion in the episode
-
-        # Remember the experience
-        agent.remember(state, action, reward, next_state, final)
-
-        # Set the current state to the new state
-        state = next_state
-
-    # Update the agent at the end of each episode
-    agent.replay(32)
-
-if suggested != 0:
-    print("accepts ", accepts)
-    print("suggested ", suggested)
+print(user)
+print("accepts ", accepts)
+print("suggested ", suggested)
 
 plt.plot(agent.losses)
 plt.xlabel('Step')
 plt.ylabel('Loss')
 plt.title('Learning Loss')
 plt.show()
-
-# # Save the trained model
-# agent.save_model("dqn_model.h5")
